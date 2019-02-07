cc51f334b06835a30e0b2109bb6ee1708e2bfab9
NoelleDL <noelle.legrain@gmail.com>
2019-02-07 21:40:49 +0000

    returns object with no undefined values

--- gradTest.js
+++ gradTest.js
@@ -2,9 +2,13 @@
 function createMenuData(data) {
 var array = [];
 var arrayOfObjects = [];
+var newArrayOfObjects = [];
   for (let i = 0; i < data.length; i++) {
-      array.push(data[i].split('/'));
-      arrayOfObjects.push( { title: array[i][0], data: [array[i][1]] } );
+    array.push(data[i].split('/'));
+    arrayOfObjects.push( { title: array[i][0], data: [array[i][1]] } );
+    if (arrayOfObjects[i].data[0] != undefined) {
+      newArrayOfObjects.push(arrayOfObjects[i]);
     }
-    return arrayOfObjects;
+  }
+    return newArrayOfObjects;
 }
