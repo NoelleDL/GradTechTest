af75b81f69f9f568f10fc5f19409755165608d98
NoelleDL <noelle.legrain@gmail.com>
2019-02-08 13:03:36 +0000

    returns correctly formatted object, need to create const to pass test

--- gradTest.js
+++ gradTest.js
@@ -3,16 +3,22 @@ function createMenuData(data) {
 
   var parents = [];
   var children = [];
+  var arrayOfObjects = [];
 
   data.forEach(function(a) {
     var split_strings  = a.split("/");
     if (split_strings[1]) {
-      if (!parents.includes(split_strings[0])) {
+      if (!parents.includes(split_strings[0]))
         parents.push(split_strings[0]);
         children.push(split_strings[1]);
       }
-    }
+    });
+
+  parents.forEach(function(p) {
+    var object = {};
+    object.title = p;
+    object.data = children.filter(c => c.includes(p));
+    arrayOfObjects.push(object);
   });
-  console.log(parents);
-  console.log(children);
+  return arrayOfObjects
 }
