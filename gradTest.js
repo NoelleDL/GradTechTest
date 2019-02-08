5bcf8a96ad663a7677e17eea257a05efa29989df
NoelleDL <noelle.legrain@gmail.com>
2019-02-08 12:48:45 +0000

    fix missing bracket and formatting

--- gradTest.js
+++ gradTest.js
@@ -1,19 +1,18 @@
 /*jshint esversion: 6 */
 function createMenuData(data) {
-var array = [];
-var arrayOfObjects = [];
-var newArrayOfObjects = [];
-var test = [];
-  for (let i = 0; i < data.length; i++) {
-    array.push(data[i].split('/'));
-    arrayOfObjects.push( { title: array[i][0], data: [array[i][1]] } );
-    if (arrayOfObjects[i].data[0] != undefined) {
-      newArrayOfObjects.push(arrayOfObjects[i]);
-          var testing = newArrayOfObjects[i].title;
-          if (newArrayOfObjects.includes(testing)) {
-            console.log("this is correct");
-        }
+
+  var parents = [];
+  var children = [];
+
+  data.forEach(function(a) {
+    var split_strings  = a.split("/");
+    if (split_strings[1]) {
+      if (!parents.includes(split_strings[0])) {
+        parents.push(split_strings[0]);
+        children.push(split_strings[1]);
       }
     }
-    return newArrayOfObjects;
+  });
+  console.log(parents);
+  console.log(children);
 }
