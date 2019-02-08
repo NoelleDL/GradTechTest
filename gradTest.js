aa605019b4e1380a62b9c9faa2056bbcb06c1d16
NoelleDL <noelle.legrain@gmail.com>
2019-02-08 11:58:18 +0000

    trying new approach

--- gradTest.js
+++ gradTest.js
@@ -3,12 +3,17 @@ function createMenuData(data) {
 var array = [];
 var arrayOfObjects = [];
 var newArrayOfObjects = [];
+var test = [];
   for (let i = 0; i < data.length; i++) {
     array.push(data[i].split('/'));
     arrayOfObjects.push( { title: array[i][0], data: [array[i][1]] } );
     if (arrayOfObjects[i].data[0] != undefined) {
       newArrayOfObjects.push(arrayOfObjects[i]);
+          var testing = newArrayOfObjects[i].title;
+          if (newArrayOfObjects.includes(testing)) {
+            console.log("this is correct");
+        }
+      }
     }
-  }
     return newArrayOfObjects;
 }
