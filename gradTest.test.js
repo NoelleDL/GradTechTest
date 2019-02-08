25a95d5100b9246e8a94b18e408e1b969d09bd30
NoelleDL <noelle.legrain@gmail.com>
2019-02-08 13:17:15 +0000

    test passing, fix spelling errors and move to test file

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,5 +1,26 @@
+/*jshint esversion: 6 */
 function createMenuData(data) {
-  
+
+  var parents = [];
+  var children = [];
+  var array_of_objects = [];
+
+  data.forEach(function(a) {
+    var split_strings  = a.split("/");
+    if (split_strings[1]) {
+      if (!parents.includes(split_strings[0]))
+        parents.push(split_strings[0]);
+        children.push(split_strings[1]);
+      }
+    });
+
+  parents.forEach(function(p) {
+    var object = {};
+    object.title = p;
+    object.data = children.filter(c => c.includes(p));
+    array_of_objects.push(object);
+  });
+  return array_of_objects;
 }
 
 describe("menu Data Generator", () => {
